ae8c10f918a491800242b636d971e739312fad5b
randi <randi65535@gmail.com>
2022-06-08 09:04:03 +0900

    String 클래스의 메소드 테스트
     replace, split, substring, chatAt 메소드에 대한 테스트 진행

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,54 @@
 package study;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.lang.invoke.MethodHandles;
+import java.util.Arrays;
+import java.util.List;
 
-public class StringTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest  {
+    public String input;
+
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() {
+        input = "1,2";
+        String[] strs = input.split(",");
+        assertThat(strs).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        input = "(1,2)";
+
+        String result = input.substring(0, input.length()-1);
+        assertThat(result).isEqualTo("(1,2");
+
+        result = result.substring(1,input.length()-1);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습")
+    @Test
+    void charAt() {
+        input = "abc";
+
+        assertThatThrownBy(() -> {
+            input.charAt(4);
+        })
+                .isInstanceOf(IndexOutOfBoundsException.class)
+                .withFailMessage("인덱스가 문자열의 범위를 넘어섰습니다.");
+    }
+
 }
